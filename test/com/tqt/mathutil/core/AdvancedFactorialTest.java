fc50899054d666d7f507ad474dff527e2b5c2613
thanhtg-it-se <falconbusterxx@Gmail.com>
2021-09-29 10:09:05 +0700

    modify code so see RED ALERT

--- test/com/tqt/mathutil/core/AdvancedFactorialTest.java
+++ test/com/tqt/mathutil/core/AdvancedFactorialTest.java
@@ -45,6 +45,8 @@ public class AdvancedFactorialTest {
                             {3, 6},
                             {4, 24},
                             {6, 720},
+                            {6, 7720},
+                            {0, 1},
                            };
   }
   @Parameter(value = 0)
